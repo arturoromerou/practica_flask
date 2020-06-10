61ae4d3dad23614f5922808a67e7973cd2dae83e
arturoromeroum <ajruwork@gmail.com>
2020-06-10 13:55:45 -0500

    se agrega la ruta completa de flask

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #nombre del app
+
+@app.route("/") # ruta del entorno
+
+def hola():
+    return('hello world!')
+
+if __name__ == '__main__': # se setea el main
+    app.run()
